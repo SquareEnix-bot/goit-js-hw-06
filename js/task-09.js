d544b31d64116ad9dbc3f0bf61e508e3e0736de3
Anastasiia Vozniuk <a.vozniuk.97@gmail.com>
2022-07-08 00:53:47 +0300

    task-9 v1.0

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+const changeBg = () => {
+  const newBGColor = getRandomHexColor();
+  colorValue.textContent = newBGColor;
+  document.body.style.backgroundColor = `${newBGColor}`;
+}
+
+changeColorBtn.addEventListener('click', changeBg)
